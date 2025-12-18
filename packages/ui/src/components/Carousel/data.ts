e5965f02429a3014b7c3e7ff98ce072c33af9ca3
SammieHuang <hzysammy@gmail.com>
2025-12-17 15:32:03 -1000

    added Carousel and Storybook

---
+++ packages/ui/src/components/Carousel/data.ts
@@ -0,0 +1,22 @@
+export const images = [
+        {
+            "src": "https://picsum.photos/seed/image10/600/400",
+            "alt":"Image 1 for carousel"
+        },
+        {
+            "src": "https://picsum.photos/seed/image2/600/400",
+            "alt":"Image 2 for carousel"
+        },
+        {
+            "src": "https://picsum.photos/seed/image9/600/400",
+            "alt":"Image 3 for carousel"
+        },
+        {
+            "src": "https://picsum.photos/seed/image4/600/400",
+            "alt":"Image 4 for carousel"
+        },
+        {
+            "src": "https://picsum.photos/seed/image5/600/400",
+            "alt":"Image 5 for carousel"
+        }
+    ]
